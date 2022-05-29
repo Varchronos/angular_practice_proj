7920fa4eb0942a8cfd56196deb47587472daa588
varchronos <babu.dhruvrajak@gmail.com>
2022-05-30 03:11:05 +0530

    adding all folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { FirstCompComponent } from './first-comp/first-comp.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    FirstCompComponent,
+    SecondCompComponent
   ],
   imports: [
     BrowserModule
